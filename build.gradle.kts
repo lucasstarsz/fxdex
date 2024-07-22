ba5960f942acef548039ba4c3ea2ca4c2b4c2891
Andrew Dey <andrewrcdey@gmail.com>
2024-07-21 20:49:13 -0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    java
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+group = "org.lucasstarsz"
+version = "0.0.1"
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+javafx {
+    version = "17"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+java {
+    modularity.inferModulePath.set(true)
+}
+
+application {
+    mainModule.set("fxdex.main")
+    mainClass.set("io.github.lucasstarsz.fxdex.App")
+}
+
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+open module fxdex.main {
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+}
