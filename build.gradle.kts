dc7d316fa0ec746d3bb1dd6d0ba91d1a790cbf39
lucasstarsz <andrewrcdey@gmail.com>
2024-07-24 16:37:59 -0400

    add license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
+/* Copyright 2024 Andrew Dey
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 plugins {
     java
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
@@ -9,7 +22,6 @@ group = "org.lucasstarsz"
 version = "0.0.1"
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
@@ -28,7 +40,6 @@ javafx {
     modules("javafx.controls", "javafx.fxml")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
@@ -43,6 +54,5 @@ application {
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
